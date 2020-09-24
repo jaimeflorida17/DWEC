76ab2418f1070c64d5bf72a5fb65eff637f0503b
Jaime Calvo <jaimecgsm97@gmail.com>
2020-09-24 14:03:54 +0200

    Subido AE1-2 y funcionando

---
+++ AE2.js
@@ -0,0 +1,5 @@
+const meses= ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+
+console.log(meses);
+
+console.table(meses);
