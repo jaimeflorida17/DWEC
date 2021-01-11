e319defbee62cff775f1585e2e6d5db59df677e7
Jaime Calvo <jaimecgsm97@gmail.com>
2021-01-11 23:35:11 +0100

    Subiendo archivos retraso

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  constructor(){}
+  
+    codigoproducto="Codigo Producto: 556446"
+    nombre="Nombre: Mclaren"
+    descripcion="Descripcion: Coche de alta gama con mas de 500cv de potencia y muy atractivo"
+    precio="Precio: 300.000€"
+    stock="Stock: 40 unidades"
+    img="assets/img/mclaren.jpg"
+  
+  
+}
